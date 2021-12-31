d87185390766591970e11ab7f0cced35f95bafac
Ben Bigelow <ben.bigelow@tutanota.com>
2021-12-31 21:16:26 +0000

    Add orders fact table

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,19 +3,19 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
 
     select
-        customer_id,
+        o.customer_id,
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as most_recent_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
+    from orders o
 
     group by 1
 
@@ -30,6 +30,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
